ee353219efa38ddf3c8db4c531df125d53678793
MD Amdadul Islam <58988171+amdadul3036@users.noreply.github.com>
2020-11-13 18:04:22 -0800

    Constant in c++

---
+++ Practices/constant.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    const int minutesPerHours = 60;
+    cout<<minutesPerHours;
+    // minutesPerHours = 50;
+    // cout<minutesPerHours;
+    // We cannot assign new value in a variable which use 'const '.
+}
