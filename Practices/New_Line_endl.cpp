038f767c0432dd1bad290fd2e5658b0995b441f4
MD Amdadul Islam <58988171+amdadul3036@users.noreply.github.com>
2020-11-13 17:49:34 -0800

    endl

---
+++ Practices/New_Line_endl.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    std::cout<<"Learning c++"<<endl;
+    std::cout<<"Programming is just a tool"<<endl;
+    std::cout<<"You have to use it to solve problems";
+
+    return 0;
+}
