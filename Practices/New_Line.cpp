1dceecb2a2b4257e2a13ef52ad26dbae2812a988
MD Amdadul Islam <58988171+amdadul3036@users.noreply.github.com>
2020-11-13 17:44:05 -0800

    Creating New Line

---
+++ Practices/New_Line.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+
+int main(){
+    std::cout<<"Learning c++ \n";
+    std::cout<<"Programming is just a tool \n\n";
+    std::cout<<"You have to use it to solve problems";
+
+    return 0;
+}
