4f7c2ca0580685e77c6b9d7467d3eee8e3f94a34
amdadul3036 <amdadul3036@gmail.com>
2020-12-24 12:43:21 +0600

    Room problem 2 solved

--- Course 1/Section 6/problem2.cpp
+++ Course 1/Section 6/problem2.cpp
@@ -7,7 +7,7 @@
     Sales tax rate is 6%
     Estimates are valid for 30 days
 
-    Prompt the user for the number of small and large rooms they would like cleaned
+    Prompt the user for the number of rooms they would like cleaned
     and provide an estimate such as:
  
 Estimate for carpet cleaning service
@@ -19,3 +19,25 @@ Tax: $3.6
 Total estimate: $63.6
 This estimate is valid for 30 days
 */
+
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int number_of_room {0};
+    int room_charge {30};
+    float room_tax {0.06};
+    int number_of_days_valid {30};
+
+    cout<<"Enter the number of room you want: "<<endl;
+    cin>>number_of_room;
+    cout<<"Number of Room You have entered Just now: "<<number_of_room<<endl;
+    cout<<"Every Room Charge: "<<room_charge<<endl;
+    cout<<"So your room charge for "<<number_of_room<<" number of rooms is: "<<room_charge*number_of_room<<endl;
+    cout<<"Tax per room: "<<room_tax<<endl;
+    cout<<"So your total room tax for "<<number_of_room<<" number of rooms is: "<<room_charge*room_tax<<endl;
+    cout<<"========================================================"<<endl;
+    cout<<"Your total rent for: "<< number_of_room << " number of room is" <<" " << ((room_charge*number_of_room) + (room_charge*room_tax)) <<endl;
+    cout<<"Thank you for your reservation."<<endl;
+}
