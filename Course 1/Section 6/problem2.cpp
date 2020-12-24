0b2b963810b39f5b865b62744891e3a3cc7b4151
amdadul3036 <amdadul3036@gmail.com>
2020-12-24 12:20:12 +0600

    Room Problem

---
+++ Course 1/Section 6/problem2.cpp
@@ -0,0 +1,21 @@
+// Section 6
+// Challenge
+
+/* 
+    Frank's Carpet Cleaning Service
+    Charges $30 per room
+    Sales tax rate is 6%
+    Estimates are valid for 30 days
+
+    Prompt the user for the number of small and large rooms they would like cleaned
+    and provide an estimate such as:
+ 
+Estimate for carpet cleaning service
+Number of rooms: 2
+Price per room: $30
+Cost : $60
+Tax: $3.6
+===============================
+Total estimate: $63.6
+This estimate is valid for 30 days
+*/
