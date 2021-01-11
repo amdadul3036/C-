992e1ff943a35c923fcbb705e5fed2e6f05c9f00
amdadul3036 <amdadul3036@gmail.com>
2021-01-11 20:21:19 +0600

    Prime Number

---
+++ Basic Problem Solving in C++/prime.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int num {};
+
+    cout << "Enter your number that you want to check if it prime or not: ";
+    cin >> num;
+
+    if (num%2==0)
+    {
+        cout << "It's Not Prime." << endl;
+    }else
+    {
+        cout << "It's Prime.";
+    }
+    
+    return 0;
+
+}
