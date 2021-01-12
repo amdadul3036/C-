aca4fbe897beab005fdfebad65d7576243cdf79b
amdadul3036 <amdadul3036@gmail.com>
2021-01-12 18:22:27 +0600

    Physic depertment problem 2

--- Physics/problem2.cpp
+++ Physics/problem2.cpp
@@ -1,2 +1,24 @@
+// 1+2+3+4+5+6+...+n = ?
+
 #include <iostream>
 using namespace std;
+
+
+int main(){
+
+    int n,i;
+
+    int sum = 0;
+
+    cout << "How many numbers do you want ot add: ";
+    cin >> n;
+
+    for ( i = 1 ; i <=n ; i++)
+    {
+        sum = sum + i;
+    }
+
+
+    cout << "Sum of the first " << n << "numbers are: " << sum << endl;
+    
+}
