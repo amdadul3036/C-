e3b227151dd5d3edf7f312c7334e744c1ec64cb4
amdadul3036 <amdadul3036@gmail.com>
2021-01-12 18:28:21 +0600

    Squate sum

--- Physics/problem3.cpp
+++ Physics/problem3.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 int main(){
+
+    int n, i , sum {} ;
+
+    cout << "Enter how many amount of number do you want to input : ";
+    cin >> n;
+
+    for ( i = 1 ; i <= n ; i++)
+    {
+        sum = sum + pow(i,2);
+    }
+
+    cout << "Expected Square Sum is : " << sum << endl;
+
+    return 0;
     
 }
