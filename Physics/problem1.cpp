59438c3e626706e322a4ec44267d65224b0b0d04
amdadul3036 <amdadul3036@gmail.com>
2021-01-12 18:10:54 +0600

    Physics Depertment Problems

---
+++ Physics/problem1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+    double a;
+
+    cout << "Enter a: ";
+    cin >> a;
+    cout << ( pow(sin(a) , 2) + pow (cos(a) , 2) ) << endl;
+
+    return 0;
+}
+
+
+// There are some problem in this code
