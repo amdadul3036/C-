e9f36603e4fd72f2d50acdd98ba3f189ea30b2da
amdadul3036 <amdadul3036@gmail.com>
2021-01-13 20:28:09 +0600

    e^-x

--- Physics/e^-x.cpp
+++ Physics/e^-x.cpp
@@ -1,2 +1,32 @@
 // e^-x = 1 - (x/1!) + ((x^2)/2!) - ((x^3)/3!) + ...... 
 
+
+#include <iostream>
+using namespace std;
+
+
+int main(){
+
+    double x;
+    int accuracy;
+
+    cout << "Enter the value of power x: ";
+    cin >> x;
+
+    cout << "How many term you want to calculate: ";
+    cin >> accuracy;
+
+    double ans {1};
+    float temp {1};
+
+    for (int i = 1; i <= accuracy ; i++)
+    {
+        temp = ( temp * (-1) * x ) / i ;
+        ans = ans + temp;
+    }
+
+    cout << "Answer is: " << ans;
+
+    return 0;
+    
+}
