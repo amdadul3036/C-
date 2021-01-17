038ff6b40d319cc1d69918a691d56ada5a7e31cb
amdadul3036 <amdadul3036@gmail.com>
2021-01-17 21:57:38 +0600

    Function

---
+++ Course 1/Section 11/function.cpp
@@ -0,0 +1,33 @@
+/*
+type function_name (arguments){
+    statement;
+};
+
+--------------------------------------
+Arguments can be different types such as --
+type function_name(int a , float b , char c){
+    statements;
+    // Body
+};
+
+--------------------------------------
+type is functinon type like int, float , void.
+*/
+
+
+// Here we just call a function and print a string
+
+#include <iostream>
+using namespace std;
+
+void dhru(void){
+    cout << " MD AMDADUL ISLAM DHRUBO" << endl ;
+}
+
+int main(){
+
+    dhru();
+    dhru();
+
+    return 0;
+}
