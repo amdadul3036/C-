09ba187acb67e48aa0e88e05a96c1dbd34e79ae2
amdadul3036 <amdadul3036@gmail.com>
2021-02-01 20:52:43 +0600

    Prime number checker

--- Basic Problem Solving in C++/Random Number/prime_number.cpp
+++ Basic Problem Solving in C++/Random Number/prime_number.cpp
@@ -1,27 +1,35 @@
 #include <iostream>
 using namespace std;
 
-int number {};
+void prime(int a);
 
 int main(){
+
+    int number {};
 
     cout << "Enter a number that you want to check if it is prime or not : " ;
     cin >> number;
 
-    int half = number/2;
-    for (int i = 2; i <= half ; i++)
-    {
-        if (number%i == 0)
-        {
-                cout <<  number << " is a prime number." << endl;
-        }else
-            {
-                cout << number << " is not a prime number." << endl;
-            }
-
-        return 0;  
-    }
-    
+    prime(number);
 }
 
+void prime(int a){
 
+    if (a == 0 || a == 1){
+        cout << a << " is not prime" << endl;
+    }
+
+    int flag_sign = 0 ;
+    for (int i = 2; i < a/2 ; i++)
+    {
+        if (a%i == 0)
+        {
+                cout <<  a << " is not a prime number." << endl;
+                flag_sign = flag_sign + 1;
+                break;
+        }
+    }
+    if(flag_sign = 0){
+         cout << a << " is a prime number." << endl;
+    }
+}  
