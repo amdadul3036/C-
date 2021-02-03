3f978b382dc190af9ba5bbe82ae91f6c6afb2e55
MD AMDADUL ISLAM <amdadul3036@gmail.com>
2021-02-03 21:16:08 +0600

    Add numbers

---
+++ Programming From NPTEL/IIT Kharagpur Course/Week 1/add_Numbers.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+    int a, b;
+    cout << "Input two numbers: " << endl;
+    cin >> a >> b;
+    int sum = a + b; // Declaration of sum
+    cout << "Sum of " << a << " and " << b << " is: " << sum << endl;
+    
+    return 0;
+}
+
+
+
