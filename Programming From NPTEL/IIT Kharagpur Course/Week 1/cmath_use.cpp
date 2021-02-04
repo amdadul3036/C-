5584f8c4b740be84fc9e9e1ea41b503afec1f3e6
MD AMDADUL ISLAM <amdadul3036@gmail.com>
2021-02-04 19:38:47 +0600

    Cmath

---
+++ Programming From NPTEL/IIT Kharagpur Course/Week 1/cmath_use.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+
+int main() {
+    double x;
+    cout << "Input number: " ;
+    cin >> x;
+    double sqrt_x = sqrt(x); // Declaration of sqrt_x
+    cout << "Square Root of " << x << " is: " << sqrt_x << endl;
+    
+    return 0;
+}
