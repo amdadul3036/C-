987fdcc137e0c76d5d8d264a76c8462e65993180
MD AMDADUL ISLAM <amdadul3036@gmail.com>
2021-02-04 20:01:20 +0600

    Bool in c++

---
+++ Programming From NPTEL/IIT Kharagpur Course/Week 1/bool.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    bool x = true;
+    bool y = false;
+    cout <<"bool is for x is " << x << endl;
+    cout <<"bool is for y is " << y << endl;
+
+    return 0;
+}
