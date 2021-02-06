47cc5e56268ca83cea4731ae2cf04ebe43f7657c
MD AMDADUL ISLAM <amdadul3036@gmail.com>
2021-02-06 22:02:05 +0600

    Compile type array

---
+++ Data Structure/Array/array1.cpp
@@ -0,0 +1,15 @@
+// Compile Type Array 
+
+#include <iostream>
+using namespace std;
+
+
+int main(){
+    
+    int array1[5] {5,3,7,9,1}; //Taking 5 bits for the array at the time of declaration; that's why it is compile type array
+
+    cout << array1[1];
+
+    return 0;
+    
+}
