07729321a8b0fa5d942617151a09a61111daee38
MD AMDADUL ISLAM <amdadul3036@gmail.com>
2021-02-09 23:36:10 +0600

    Area of circle with cmath

---
+++ Basic Problem Solving in C++/circle_area2.cpp
@@ -0,0 +1,18 @@
+// Write a code to find the area of a circle 
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+
+    double radius , area;
+
+    cout << "Enter the radius: ";
+    cin >> radius;
+
+    area = pow(radius,2) * 3.1416;
+
+    cout << "Area is : " << area;
+    return 0;
+}
