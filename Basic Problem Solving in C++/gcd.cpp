7121a65483ee76b9037492f14bb917facb8385c2
MD AMDADUL ISLAM <amdadul3036@gmail.com>
2021-02-09 22:53:14 +0600

    GCD Rules

---
+++ Basic Problem Solving in C++/gcd.cpp
@@ -0,0 +1,18 @@
+/*
+Find the GCD of the given values
+GCD is also known as HCF.
+
+RULES:
+1. Find all dividors
+2. Find all common dividors
+3. Find the largest common dividor
+
+Example: 
+12 =  1 , 2 , 3 , 4 , 6 , 12
+16 =  1 , 2 , 4 , 16
+
+So common is here {1,2,4}
+And largest common dividor is 4.
+
+So the GCD of 12 and 16 is 4.
+*/
