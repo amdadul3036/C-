8ceeb299c7c5d3814e7331898ac73269181036ea
MD AMDADUL ISLAM <amdadul3036@gmail.com>
2021-02-09 23:22:36 +0600

    GCD/HCM

--- Basic Problem Solving in C++/gcd.cpp
+++ Basic Problem Solving in C++/gcd.cpp
@@ -16,3 +16,32 @@ And largest common dividor is 4.
 
 So the GCD of 12 and 16 is 4.
 */
+
+
+#include<iostream>
+using namespace std;
+
+
+int main(){
+    int num1 , num2 , gcd;
+
+    cout << "Enter first number : ";
+    cin >> num1 ;
+
+    cout << "Enter second number : ";
+    cin >> num2 ;
+
+    for (int i = 1 ; i < num1 && i< num2; i++) //Notice that we do not need to cross it because GCD will come in this range. Use the example above 12 and 16 to understand it clearly
+    {
+        if (num1%i == 0 && num2%i == 0)
+        {
+            gcd = i;
+        }
+        
+    }
+
+    cout << "GCD : " << gcd << endl;
+
+    return 0;
+    
+}
