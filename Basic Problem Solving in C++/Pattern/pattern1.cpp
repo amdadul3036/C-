dbb540702f00529c1b953354d43e84991e0b7911
MD AMDADUL ISLAM <amdadul3036@gmail.com>
2021-02-16 17:00:15 +0600

    Pattern 1

---
+++ Basic Problem Solving in C++/Pattern/pattern1.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n,row, column ;
+
+    cout << "Enter how many * do you want : ";
+    cin >> n;
+
+    for (int i = 1 ; i < row; i++)
+    {
+        for (int j = 1; j < n; j++)
+        {
+            cout << "* " ;
+        }
+
+        cout << endl;
+        
+    }
+    
+}
