a34580e15ae9e74ef3789ca7de22472a689d802c
MD AMDADUL ISLAM <amdadul3036@gmail.com>
2021-02-22 21:58:53 +0600

    UNSOLVED PROBLEM

--- Basic Problem Solving in C++/divisible_by_two.cpp
+++ Basic Problem Solving in C++/divisible_by_two.cpp
@@ -16,22 +16,20 @@ using namespace std;
 int main(){
     int number , calculation_number , count {};
 
-    cout << "Enter the number that you want to check: "; //16
-    cin >> number; //16
+    cout << "Enter the number that you want to check: "; 
+    cin >> number; 
 
-    calculation_number = number ;  //16
+    calculation_number = number ;  
 
-    for (int i = 1; i <= number/2; i++) //8
+    while (calculation_number%2==0)
     {
-        if (calculation_number%2==0) //16 8
-        {
-            count +=1;  //1 2 3 4
-        }
-        calculation_number = calculation_number/2; // 8  4  2
+        count +=1; 
+        calculation_number = calculation_number/2; 
     }
-    
+
     cout << count;
 
     return 0;
 }
 
+// CANNOT SOLVE THIS PROBLEM. TRIED THREE DAYS MY GOSSSS
