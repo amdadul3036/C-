b93929d731ec0825a48813c6b8da159b7383d815
MD AMDADUL ISLAM <amdadul3036@gmail.com>
2021-02-24 17:56:41 +0600

    Max min new version

---
+++ Basic Problem Solving in C++/max_min_diff.cpp
@@ -0,0 +1,38 @@
+/*
+You will take several integer numbers as inputs until the user enters -1. Then calculate the
+difference between the Highest and Lowest number.
+
+    EXAMPLE: 
+
+        Input               Output
+        7 5 1 9 5 -1          8
+        0 7 8 4 9 -1          9
+*/
+
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int number , arr[50] ;
+
+    for (int i = 0; i < 50; i++)
+    {
+        cout << "Enter the number : ";
+        cin >> number;
+
+        if (number != -1)
+        {
+            cin >> arr[i];
+        }else if (number == -1)
+        {
+            cin >> arr[i];
+        }
+        
+    }
+    
+    
+    // cout << arr ;
+    
+    
+}
