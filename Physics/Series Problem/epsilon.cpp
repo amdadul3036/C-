3025a1ada36e3fa83437a98c50199726125f0658
MD AMDADUL ISLAM <amdadul3036@gmail.com>
2021-03-03 23:37:40 +0600

    Series

---
+++ Physics/Series Problem/epsilon.cpp
@@ -0,0 +1,38 @@
+/*
+Write a C program that estimates the value of the mathematical constant E(EPSILON) by using the formula:
+
+E = 1 + (1/1!) + (1/2!) + (1/3!) + (1/4!) + .....................
+*/
+
+#include<iostream>
+using namespace std;
+
+void Facto();
+
+int main(){
+
+    int terms;
+
+    cout << "Enter how many terms do you want to calculate: ";
+    cin >> terms;
+
+    for (int i = 0; i <= terms; i++)
+    {
+        /* code */
+    }
+}
+
+void Facto(){
+
+    int terms;
+
+    cout << "Enter the number: ";
+    cin >> terms;
+
+    if (terms == 0 || terms == 1)
+    {
+        /* code */
+    }
+    
+
+}
