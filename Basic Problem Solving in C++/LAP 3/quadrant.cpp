797f22b57ca97934c0a4dc1a62b0d001052dedf6
MD AMDADUL ISLAM <amdadul3036@gmail.com>
2021-03-04 22:51:06 +0600

    Quadrant

---
+++ Basic Problem Solving in C++/LAP 3/quadrant.cpp
@@ -0,0 +1,9 @@
+/*
+Write a C program which takes two integer values X and Y as inputs and determines which quadrant the point
+lies.
+Sample Input/Output:
+Input           Output
+5  7        First Quadrant
+-1 3        Second Quadrant
+0  0            Origin
+*/
