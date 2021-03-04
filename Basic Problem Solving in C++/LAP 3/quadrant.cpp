e62f0f0f9569c73714e59fb73a7d1328294aaecc
MD AMDADUL ISLAM <amdadul3036@gmail.com>
2021-03-04 22:56:34 +0600

    Quadrant problem solved

--- Basic Problem Solving in C++/LAP 3/quadrant.cpp
+++ Basic Problem Solving in C++/LAP 3/quadrant.cpp
@@ -7,3 +7,35 @@ Input           Output
 -1 3        Second Quadrant
 0  0            Origin
 */
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    double x,y;
+
+    cout << "Enter x : ";
+    cin >> x;
+
+    cout << "Enter y : ";
+    cin >> y;
+
+    if (x>0 & y>0)
+    {
+        cout << "First Quadrant";
+    }else if (x<0 & y>0)
+    {
+        cout << "Second Quadrant";
+    }else if (x<0 & y<0)
+    {
+        cout << "Third Quadrant";
+    }else if (x>0 & y<0)
+    {
+        cout << "Forth Quadrant";
+    }else
+    {
+        cout << "Origin";       
+    }
+    
+    return 0;
+}
