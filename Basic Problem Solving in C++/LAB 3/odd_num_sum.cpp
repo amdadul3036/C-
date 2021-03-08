e6263522fa55c9b087ef5cf577aca4c92e92a5ec
MD AMDADUL ISLAM <amdadul3036@gmail.com>
2021-03-08 22:23:58 +0600

    Rearrange

--- Basic Problem Solving in C++/LAB 3/odd_num_sum.cpp
+++ Basic Problem Solving in C++/LAB 3/odd_num_sum.cpp
@@ -2,11 +2,11 @@
 Write a program that uses a WHILE LOOP to calculate the sum of all the odd numbers entered by the user.
 Assume the last value read is the sentinel -1.
 
-EXAMPLE:
+    EXAMPLE:
 
-        Input                           Output
-        1 2 3 4 5 6 7 8 9 10 -1           25
-        2 4 7 9 12 4 4 7 -1               23
+            Input                           Output
+            1 2 3 4 5 6 7 8 9 10 -1           25
+            2 4 7 9 12 4 4 7 -1               23
 */
 
 #include <iostream>
