b206b98865fcd1155c3ac114694220830263c5a4
MD AMDADUL ISLAM <amdadul3036@gmail.com>
2021-03-10 23:49:54 +0600

    Unsolved

--- Basic Problem Solving in C++/factorial2.cpp
+++ Basic Problem Solving in C++/factorial2.cpp
@@ -8,3 +8,28 @@ Input           Output
 5                120
 -1
 */
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n , fact_result;
+
+    cout << "Enter the number : ";
+    cin >> n;
+
+    if (n == 1 || n == 0)
+    {
+        fact_result = 1 ;
+    }else{
+        if (n ==)
+        {
+            /* code */
+        }
+        
+    }
+    
+
+    cout << fact_result;
+    
+}
