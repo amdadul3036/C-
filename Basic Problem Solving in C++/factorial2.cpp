2a1854fef6e98fd491fcaffe0c134a96e3f9bd5f
MD AMDADUL ISLAM <amdadul3036@gmail.com>
2021-03-10 23:42:51 +0600

    Factorial

---
+++ Basic Problem Solving in C++/factorial2.cpp
@@ -0,0 +1,10 @@
+/*
+Find the factorial of a given number. Terminate the program if user enters -1.
+Sample Input/Output:
+Input           Output
+0                 1
+1                 1
+2                 2
+5                120
+-1
+*/
