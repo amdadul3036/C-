dcc8e81cc9966f0380dc63424eb7465d531943fb
MD AMDADUL ISLAM <amdadul3036@gmail.com>
2021-03-12 23:53:40 +0600

    Pattern

---
+++ Basic Problem Solving in C++/Pattern/pattern6.cpp
@@ -0,0 +1,20 @@
+/*
+Draw the following shape having the number of rows input by the user (Following shape is for input 5):
+
+
+        * * * * *
+          * * * *
+            * * *
+              * *
+                *
+*/
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+
+    cout << "Enter the number of row : ";
+    cin >> n;
+}
