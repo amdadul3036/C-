156e08509f8958728e71c6812f51c34b0c438ea6
MD AMDADUL ISLAM <amdadul3036@gmail.com>
2021-04-06 23:43:37 +0600

    Enumeration

--- Intermediate to Advance/enumeration.cpp
+++ Intermediate to Advance/enumeration.cpp
@@ -6,11 +6,18 @@ enum WAPONE //Generally the name of Enumeration be CAPITALIZED
     MISSILE = 5,TORPEDO = 8 , CANNON = 9 ,ANTIAIRCRAFTGUN,RAILGUN  //This elements are known as "Enumerator".Last one do not have any comma
 };
 
+enum WAPON_2
+{
+   BOFORRO,
+   CANNON_2,
+   TORPEDO  //This is an Error because in WAPON enumeration we already have "TORPEDO"; You cannot use it again in another enumeration under same code
+};
+
 int main(){
     //There are three types of expression for Enumeration such as --- 
 
     WAPONE Messile = MISSILE;
-    WAPONE Torpedo(TORPEDO);
+    WAPONE Torpedo(TORPEDO);   //This is an Error because in WAPON enumeration we already have "TORPEDO"; You cannot use it again in another enumeration under same code
     WAPONE Cannon{CANNON};
 
 
