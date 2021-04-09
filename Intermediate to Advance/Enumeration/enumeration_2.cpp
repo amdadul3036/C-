68fe706626a865341dfcc9c7e8254c3529f9e40e
MD AMDADUL ISLAM <amdadul3036@gmail.com>
2021-04-09 23:33:52 +0600

    ABC

--- Intermediate to Advance/Enumeration/enumeration_2.cpp
+++ Intermediate to Advance/Enumeration/enumeration_2.cpp
@@ -14,7 +14,7 @@ int main(){
 
     // cin >> Missile; //It will give an error. Because we cannot input value in this way in Enumeration
 
-    cin >> Missilevalue;
+    cin >> Missilevalue ;
 
     WAPONE Missile_2 = static_cast<WAPONE>(Missilevalue);
     cout << Missile_2;
