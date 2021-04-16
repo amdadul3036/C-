4501800289ca8f56b4ca773fbc57d70e86318ae9
MD AMDADUL ISLAM <amdadul3036@gmail.com>
2021-04-16 18:27:07 +0600

    Enumeration 3

--- Intermediate to Advance/Enumeration/enumeration_3.cpp
+++ Intermediate to Advance/Enumeration/enumeration_3.cpp
@@ -6,6 +6,19 @@ enum WAPONE
     MISSILE = 5,TORPEDO = 8 , CANNON = 9 ,ANTIAIRCRAFTGUN,RAILGUN  
 };
 
+
+void printWaponeName(WAPONE wapone){
+    switch (wapone)
+    {
+    case /* constant-expression */:
+        /* code */
+        break;
+    
+    default:
+        break;
+    }
+}
+
 int main(){
     WAPONE Cannon = CANNON;  //9
 
