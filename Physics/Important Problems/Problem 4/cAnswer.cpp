56e678d24218b6184ee90a868303471ed591e16f
MD AMDADUL ISLAM <amdadul3036@gmail.com>
2022-01-10 22:07:04 +0600

    cAnswer First part

---
+++ Physics/Important Problems/Problem 4/cAnswer.cpp
@@ -0,0 +1,20 @@
+
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+double hermite(double x, int n){
+    if (n == 0) return 1;
+    else if (n == 1) return 2*x; 
+
+    else return ((2*x*hermite(x, n-1)) - (2*(n-1)*hermite(x, n-2)));
+}
+
+int main(){
+    for (double x = -3; x <= 3; x+=0.01)  // Question e bola nai koto kore increament korbo tai 0.01 kore korlum hehe
+    {
+        cout << x << " " << hermite(x,0) <<" "<< hermite(x,1) <<" "<< hermite(x,2) <<" "<< hermite(x,3) <<" "<< hermite(x,4) << endl; 
+    }
+    
+    return 0; 
+}
