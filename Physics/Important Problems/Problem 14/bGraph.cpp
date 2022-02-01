4fdda4dcaa2115ce6ecaa5bb982987348cc44e6f
MD AMDADUL ISLAM <amdadul3036@gmail.com>
2022-02-01 14:14:45 +0600

    bGraph

---
+++ Physics/Important Problems/Problem 14/bGraph.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<cmath>
+#include<fstream>
+using namespace std;
+
+double legend(double x, int n){
+    if(n==0) return 1;
+    else if(n==1) return x;
+
+    else return (((((2*n) -  1)*x*legend(x, n-1)) - (((n-1)*legend(x, n-2))))/n);
+}
+
+int main(){
+    ofstream fout("legend.dat");
+    for(double x = -1; x<=1; x+=0.01){
+        fout<< legend(x,0) << " " << legend(x,1) << " " << legend(x,2) << " " << legend(x,3) << " " << legend(x,4) << endl; 
+    }
+
+    return 0;
+}
