e8820fc9fbd967901275f190806191afae1449c5
MD AMDADUL ISLAM <amdadul3036@gmail.com>
2022-02-01 18:02:06 +0600

    Graph 21

---
+++ Physics/Important Problems/Problem 21/cGraph.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<cmath>
+#include<fstream>
+using namespace std;
+
+double invtan(double x, int n){
+    double result = 0.0;
+
+    for(int i=0;i<=n; i+=1){
+        result += pow(-1,i)*pow(x,((2*i)+1))/((2*i)+1);
+    }
+    return result;
+}
+
+int main(){
+
+    ofstream fout("invtan.dat");
+    for(int n=10;n<=1000;n++){
+        fout << n << " " << abs(invtan(1,n) - atan(1)) << endl;
+    }
+}
