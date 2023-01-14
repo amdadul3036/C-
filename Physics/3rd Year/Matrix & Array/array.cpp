6cdb5d34106c7e8164625358585a802c72f63f2b
MD AMDADUL ISLAM <amdadul3036@gmail.com>
2023-01-14 22:58:35 +0600

    3rd Year Array Start

---
+++ Physics/3rd Year/Matrix & Array/array.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int A[5] = {2,4,5,2,1};
+
+    cout << "Value of 1st Element of Array: "<< A[0]<<endl;  //It will print the index number 0 which is 2.
+    cout << "Value of 3rd Element of Array: "<< A[2]<<endl;  //It will print the index number 2 which is 5.
+}
