21313c1fc47fff7a3e8623fae02ab8fbec5ff207
MD AMDADUL ISLAM <amdadul3036@gmail.com>
2023-02-11 22:57:44 +0600

    Trap

---
+++ Physics/3rd Year/Numerical Integrations/Trapezoid Rule/2/trap.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+#include<cmath>
+
+double n,a;
+#define f(x) pow(x,n)*exp((-1)*a*pow(x,2))
+
+using namespace std;
+
+int main(){
+    double lowerLimit, upperLimit, Integrations = 0.0, subintervalNumber, stepsize,k;
+
+    cout<<"Enter the Lower Limit of the Integration: ";
+    cin>> lowerLimit;
+
+    cout<<"Enter the Upper Limit of the Integration: ";
+    cin>> upperLimit;
+
+    cout<<"Enter number of subintervals: ";
+    cin>> subintervalNumber;
+
+    //Step Size Calculation
+    stepsize = (upperLimit-lowerLimit)/subintervalNumber;
+
+    //Finding Integral Value
+
+    Integrations = f(upperLimit)+f(lowerLimit);
+    for (int i = 1; i < subintervalNumber - 1; i++)
+    {
+        k = lowerLimit + (i*stepsize);
+        Integrations += (2*f(k));
+    }
+    
+    Integrations = (stepsize/2)*Integrations;
+
+    cout<<"Value of the integration is: "<<Integrations<<endl;
+
+    return 0;
+}
+
+
+//ekhane amar question holo ; a & n er value kivabe pelo
