ac94df16e4edc62daa8d71256233b26498629357
MD AMDADUL ISLAM <amdadul3036@gmail.com>
2023-05-16 21:56:26 +0600

    Runge Kutta Method

---
+++ Physics/3rd Year/ODE/Runge Kutta Method/RK 1/rk1.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<cmath>
+#include<fstream>
+using namespace std;
+
+ofstream fout('rk1.dat');
+double f(double t, double x){
+    return t*sqrt(x);
+}
+
+double RK(double h, double t0, double x0){
+    double k1 = h*f(t0,x0);
+    double k2 = h*f(t0+(h/2),x0+(k1/2));
+    double k3 = h*f(t0+(h/2),x0+(k2/2));
+    double k4 = h*f(t0+h,x0+k3);
+
+    return x0 + ((k1+(2*k2)+(2*k3)+k4)/6);
+}
+
+int main(){
+    ofstream fout("rk1.dat");
+    double t = 0, x=1, dt=0.1;
+
+    for ( t = 0; t <= 10; t+=dt)
+    {
+        fout<<t<<" "<<x<<" "<<(pow(pow(t,2),2))/16<<endl;
+        x = RK(dt,t,x);
+    }
+
+    return 0;
+    
+}
